292a683aa9c9a72f9936b0691924f6627c08f8ec
lulujal <jaluadhi19@students.unnes.ac.id>
2025-02-10 15:42:37 +0700

    tidak jadi

--- config/config.js
+++ config/config.js
@@ -1,18 +1,18 @@
 require('dotenv').config()
 module.exports = {
     development: {
-        // "username" : process.env.PGUSER,
-        // "password" : process.env.PGPASSWORD,
-        // "database" : process.env.PGDATABASE,
-        // "host" : process.env.PGHOST,
-        // "port" : process.env.PGPORT,
-        // "dialect" : "postgres",
-
-        "username" : "postgres",
-        "password" : "admin",
-        "database" : "cctvdev",
-        "host" : "127.0.0.1",
+        "username" : process.env.PGUSER,
+        "password" : process.env.PGPASSWORD,
+        "database" : process.env.PGDATABASE,
+        "host" : process.env.PGHOST,
+        "port" : process.env.PGPORT,
         "dialect" : "postgres",
+
+        // "username" : "postgres",
+        // "password" : "admin",
+        // "database" : "cctvdev",
+        // "host" : "127.0.0.1",
+        // "dialect" : "postgres",
     },
     test: {
         "username" : "postgres",
