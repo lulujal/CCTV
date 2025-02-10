7d62b1c2a71a65d629e5d1d37d0679aa1e444a75
lulujal <jaluadhi19@students.unnes.ac.id>
2025-02-10 14:54:03 +0700

    dev cctv banyak

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ module.exports = {
 
         "username" : "postgres",
         "password" : "admin",
-        "database" : "cctv",
+        "database" : "cctvdev",
         "host" : "127.0.0.1",
         "dialect" : "postgres",
     },
