09cb54dda968ea1e656c2ffa2f5a841e93df0945
lulujal <jaluadhi19@students.unnes.ac.id>
2025-02-23 20:55:45 +0700

    http/https

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const app = require('./app');
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "upgrade-insecure-requests");
+  next();
+});
 app.listen(2443, () => {
     console.log(`App is running at 2443 / in ${app.get('env')} mode`);
   });
